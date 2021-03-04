f728bc0f5ce10e14efd1275f1d92e60b627b2fef
richaheris <>
2021-03-03 19:20:46 -0500

    adding files

---
+++ bartmossCollectiveText.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+reality := "first will come the great reality check, which ushers in the future of the world."
+victim := "whenever money changes hands, your true self becomes a victim to capitalism."
+meditation := "the best weapon against capitalism is uninterrupted meditation."
+oppression := "oppression will only end once you stop oppressing your self."  
+
+the_great_dic[reality, victim, meditation, oppression]
+
+func main() {
+  fmt.Printf(random.the_great_dic)
+  
